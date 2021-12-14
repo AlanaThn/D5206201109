51e2897e8077b51f740e1316ad56df510d545df1
AlanaThn <alanathufailah.205026@mhs.its.ac.id>
2021-12-14 16:09:12 +0700

    update css

--- resources/views/baju/edit.blade.php
+++ resources/views/baju/edit.blade.php
@@ -6,33 +6,69 @@
 
 @section('konten')
 
-<a href="/baju"> Kembali</a>
+<style>
+    input[type=text], select {
+      width: 50%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
 
-<br/>
-<h3>Edit Data Baju</h3>
-<br/>
+    input[type=number], select {
+      width: 50%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+    </style>
 
-	@foreach($baju as $b)
-	<form action="/baju/update" method="post">
-		{{ csrf_field() }}
-		<input type="hidden" name="kodebaju" value="{{ $b->kodebaju }}">
+<body>
 
-        <div class="container">
-
-		Merk Baju <input type="text" required="required" name="merk" value="{{ $b->merkbaju }}"> <br/>
-		Stock Baju <input type="number" required="required" name="stock" value="{{ $b->stockbaju }}"> <br/>
-
-        </div>
-
-        Status
-        <input type="radio" id="ada" name="status" value="A @if($b->tersedia==='H') checked="checked" @endif>
-        <label for="ada">AVAILABLE</label><br>
-        <input type="radio" id="habis" name="status" value="E" @if($b->tersedia==='A') checked="checked" @endif>
-        <label for="habis">EMPTY</label><br>
+    <a href="/baju" style="margin-left:15px;" class="btn btn-info btn-sm " role="button"> Kembali</a>
 
 
-		<input type="submit" value="Simpan Data">
-	</form>
-	@endforeach
+    <br>
+    <h3 style="margin-left:10px;">Edit Data Baju</h3>
+    <br>
 
-    @endsection
+    <div class="container">
+        @foreach ($baju as $b)
+            <form action="/baju/update" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="kodebaju" value="{{ $b->kodebaju }}">
+
+                <div class="form-group">
+                    <label for="merk">Merk Baju :</label>
+                    <input type="text" class="form-control" required="required" name="merk" value="{{ $b->merkbaju }}">
+
+                </div>
+                <div class="form-group">
+                    <label for="merk">Stock Baju :</label>
+                   <input type="number" class="form-control" required="required" name="stock" value="{{ $b->stockbaju }}"> <br />
+
+                </div>
+
+                <div class="form-group">
+                    <label for="status">Status :</label>
+                <input type="radio" id="ada" name="status" value="A @if ($b->tersedia === 'H') checked="checked" @endif>
+            <label for=" ada">AVAILABLE</label>
+                <input type="radio" id="habis" name="status" value="E" @if ($b->tersedia === 'A') checked="checked" @endif>
+                <label for="habis">EMPTY</label>
+            </div>
+
+            <br>
+            <br>
+
+                <input type="submit" value="Simpan Data" class="btn btn-success btn-sm " role="button">
+            </form>
+    </div>
+    @endforeach
+
+</body>
+@endsection
